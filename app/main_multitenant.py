c4fb171806d4cb2787297c37226642f60a1eea76
Stacey <staceynyambura6@gmail.com>
2026-04-16 22:46:39 +0300

    type change in main_mulittenant.py

--- app/main_multitenant.py
+++ app/main_multitenant.py
@@ -45,7 +45,7 @@ DEFAULT_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "waterbillsystem-three.vercel.app",
+    "http://waterbillsystem-three.vercel.app",
 ]
 ALLOWED_ORIGINS = [
     origin.strip()
